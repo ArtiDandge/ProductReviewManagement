730a4e5d116359a01820aeba4f6c7deb202f402f
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2021-01-04 18:16:26 +0530

    [Arti Dandge] Add : Ability to find average rating for each product id

--- ProductReviewManagement/ProductReviewDataTable.cs
+++ ProductReviewManagement/ProductReviewDataTable.cs
@@ -38,21 +38,21 @@ namespace ProductReviewManagement
             table.Rows.Add(13, 14, 2, "Not Good", false);
             table.Rows.Add(14, 15, 5, "Good", true);
             table.Rows.Add(15, 16, 2.5, "Good", true);
-            table.Rows.Add(16, 17, 9, "Nice", true);
+            table.Rows.Add(15, 17, 9, "Nice", true);
             table.Rows.Add(17, 18, 7, "Good", true);
-            table.Rows.Add(18, 19, 10, "Nice", true);
+            table.Rows.Add(17, 19, 10, "Nice", true);
             table.Rows.Add(19, 20, 5, "Good", true);
             table.Rows.Add(20, 21, 5, "Good", true);
-            table.Rows.Add(21, 22, 1.3, "Not Good", false);
+            table.Rows.Add(20, 22, 1.3, "Not Good", false);
             table.Rows.Add(22, 23, 4, "Good", true);
-            table.Rows.Add(23, 24, 2, "Not Good", false);
+            table.Rows.Add(22, 24, 2, "Not Good", false);
             table.Rows.Add(24, 24, 9, "Nice", true);
             table.Rows.Add(25, 25, 7, "Nice", true);
             return table;
         }
    
         /// <summary>
-        /// Method To Display DataTable records whose isLike values is true
+        /// Method To Display DataTable records who's isLike values is true
         /// </summary>
         /// <param name="table"></param>
         public void DisplayDataTableRecordsWithIsLikeValueTrue(DataTable table)
@@ -64,6 +64,28 @@ namespace ProductReviewManagement
             {
                 Console.WriteLine("\n-----------------");
                 Console.Write("\nProductID : " + row.Field<int>("ProductID")+" "+ "\nUserID : " + row.Field<int>("UserID") + " "+ "\nRating : " + row.Field<float>("Rating") + " "+ "\nReview : " + row.Field<string>("Review") + " "+ "\nisLike : "+ row.Field<bool>("isLike") + " ");
+                Console.WriteLine("\n-----------------");
+            }
+        }
+
+        /// <summary>
+        /// Method to Find Average Rating for Each Product ID
+        /// </summary>
+        /// <param name="table">table</param>
+        public void FindAverageRatingOfEachProductID(DataTable table)
+        {
+            var records = table.Rows.Cast<DataRow>()
+                          .GroupBy(x => x.Field<int>("ProductID"))
+                          .Select(x => new
+                          {
+                             ProductID = x.Key,
+                             Average = x.Average(x => x.Field<float>("Rating"))
+                          }).ToList();
+            Console.WriteLine("\nList of Average Rating For Given Each Product ID");
+            foreach (var row in records)
+            {
+                Console.WriteLine("\n-----------------");
+                Console.Write("\nProductID : " + row.ProductID + " " + "\nAverage Rating : " + row.Average);
                 Console.WriteLine("\n-----------------");
             }
         }
--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -56,6 +56,7 @@ namespace ProductReviewManagement
             ProductReviewDataTable reviewDataTable = new ProductReviewDataTable();
             DataTable table = reviewDataTable.CreateDataBleAndAddDefaultValues();
             reviewDataTable.DisplayDataTableRecordsWithIsLikeValueTrue(table);
+            reviewDataTable.FindAverageRatingOfEachProductID(table);
         }
     }
 }
